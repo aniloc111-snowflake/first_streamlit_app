583c83c81d3acc98d83ac65ed91fea73aa2926f9
aniloc111-snowflake <130879429+aniloc111-snowflake@users.noreply.github.com>
2023-04-17 15:38:12 +0800

    Update streamlit_app.py
    library imports are moved at the beginning; added stop for troubleshooting

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,8 @@
 import streamlit
+import pandas
+import requests
 import snowflake.connector
+from urllib.error import URLError
 
 streamlit.title('My Parents New Healthy Diner')
 streamlit.header('Breakfast Menu')
@@ -10,7 +13,6 @@ streamlit.text('🥑 🍞 Avocado Toast')
 
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
-import pandas
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 my_fruit_list = my_fruit_list.set_index('Fruit')
 
@@ -24,8 +26,6 @@ streamlit.dataframe(fruits_to_show)
 ## REQUESTS
 streamlit.header('Fruityvice Fruit Advice')
 
-import requests
-
 fruit_choice = streamlit.text_input('What fruit would you like information about?', 'Kiwi')
 streamlit.write('The user entered ', fruit_choice)
 
@@ -37,6 +37,9 @@ fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 
 # output in streamlit
 streamlit.dataframe(fruityvice_normalized)
+
+# don't run anything past here while troubleshooting
+streamlit.stop()
 
 ## using snowflake connector
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
