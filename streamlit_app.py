25ed5ed215121e2520f1bbb6b2faec079c50cc16
aniloc111-snowflake <130879429+aniloc111-snowflake@users.noreply.github.com>
2023-04-17 15:34:03 +0800

    Update streamlit_app.py
    adding insert query

--- streamlit_app.py
+++ streamlit_app.py
@@ -29,9 +29,6 @@ import requests
 fruit_choice = streamlit.text_input('What fruit would you like information about?', 'Kiwi')
 streamlit.write('The user entered ', fruit_choice)
 
-add_my_fruit = streamlit.text_input('What fruit would you like to add?')
-streamlit.write('Thanks for adding ', add_my_fruit)
-
 # fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + "kiwi")
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
 
@@ -49,3 +46,10 @@ my_cur.execute("select * from fruit_load_list")
 my_data_rows = my_cur.fetchall()
 streamlit.header("The fruit load list contains:")
 streamlit.dataframe(my_data_rows)
+
+## second input box
+add_my_fruit = streamlit.text_input('What fruit would you like to add?')
+streamlit.write('Thanks for adding ', add_my_fruit)
+
+## adding input in the db
+my_cur.execute("insert into fruit_load_list values ('from streamlit')")
